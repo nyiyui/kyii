2701aa1107e191c45c3d516b18b5f4e65d56225b
Ken Shibata <kenxshibata@gmail.com>
2022-06-16 10:17:50 -0400

    yuui: fallback for profile image

--- yuui/src/lib/iori/User.svelte
+++ yuui/src/lib/iori/User.svelte
@@ -1,11 +1,23 @@
 <script lang="ts" type="module">
 	import { _ } from 'svelte-i18n'
 	import { client } from '$lib/api2'
+	import Icon from '@iconify/svelte'
+	import { onMount } from 'svelte'
 
 	export let uid: string
 	export let name: string
 	export let slug: string
 	export let iconOnly = false
+	let iconFound = true
+
+	onMount(async() => {
+		const url = new URL(`api/v2/user/${uid}/img`, client.baseUrl)
+		const r = await fetch(url.toString())
+		iconFound = r.status !== 404
+		console.log(`iconFound`, iconFound);
+	})
+
+	// TODO: optimize to only one fetch
 </script>
 
 {#if uid == 'anonymous' || uid === null}
@@ -14,11 +26,15 @@
 	</span>
 {:else}
 	<span class="user">
-		<img
-			alt={$_('iori.user.profile')}
-			class="user-img"
-			src={new URL(`api/v2/user/${uid}/img`, client.baseUrl).toString()}
-		/>
+		{#if iconFound}
+			<img
+				alt={$_('iori.user.profile')}
+				class="user-img"
+				src={new URL(`api/v2/user/${uid}/img`, client.baseUrl).toString()}
+			/>
+		{:else}
+			<Icon icon="mdi:account-circle" class="user-img" />
+		{/if}
 		{#if !iconOnly}
 			<div class="non-img">
 				<div class="name">{name}</div>
