702353b43f8ee8f26eeed4b4f63f7bbc70337af2
Ken Shibata <kenxshibata@gmail.com>
2022-06-29 00:40:29 -0400

    yuui: add stuff to index

--- yuui/src/routes/index.svelte
+++ yuui/src/routes/index.svelte
@@ -1,8 +1,22 @@
 <script lang="ts" type="module">
+	import { _ } from 'svelte-i18n'
+	import { ulos as ulosStore, currentUlid } from '$lib/api2'
+	import User from '$lib/iori/User.svelte'
+
+	let ulo = $ulosStore.get($currentUlid)
 </script>
 
 <svelte:head>
 	<title>Yuui</title>
 </svelte:head>
 
-<main />
+<main>
+	{$_('index.logged_in_as')}
+	{#if ulo}
+		<User
+			uid={ulo.uid}
+			name={ulo.name}
+			slug={ulo.slug}
+		/>
+	{/if}
+</main>
