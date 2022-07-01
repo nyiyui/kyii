418514c686b9139ba606aa2ec1b2adae1a4e84aa
Ken Shibata <kenxshibata@gmail.com>
2022-07-01 01:25:40 -0400

    gha/yuui: add stubs

--- airy/airy/routes/util.py
+++ airy/airy/routes/util.py
@@ -3,6 +3,9 @@ from pathlib import Path
 from PIL import Image
 
 
-def conv_to_webp(src: Path, dst: Path):
+def conv_to_webp(src: Path, dst: Path) -> None:
+    """
+    Convert image at src to webp at dst.
+    """
     img = Image.open(src)
     img.save(dst, format="webp")
